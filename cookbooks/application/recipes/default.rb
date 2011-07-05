4f55529bddd07194073d0e724b2530182e56cb84
Rudiger Lippert <rudy.lippert@originatelabs.com>
2011-07-05 10:40:42 -0700

    Moved a lot of the recipe details from the rails recipe to the application recipe

--- cookbooks/application/recipes/default.rb
+++ cookbooks/application/recipes/default.rb
@@ -1,5 +1,56 @@
+#--------------------------------------------------------------------------------
+# Required recipes
+#--------------------------------------------------------------------------------
+require_recipe "apt"
+require_recipe "build-essential"
+require_recipe "mysql::server"
+require_recipe "apache2"
 require_recipe "rails"
+require_recipe "passenger_apache2::mod_rails"
 
+#--------------------------------------------------------------------------------
+# Required packages
+#--------------------------------------------------------------------------------
+package "zip"
+package "libxslt1-dev"
+
+#--------------------------------------------------------------------------------
+# Rake incompatability fix
+#--------------------------------------------------------------------------------
+bash "downgrade-rake" do
+  code "gem uninstall rake -v\"~>0.9\""
+
+  only_if do
+    `gem list rake`.include?('0.9')
+  end
+end
+
+#--------------------------------------------------------------------------------
+# Gems
+#--------------------------------------------------------------------------------
+gem_package "bundler"
+
+execute "install bundle" do
+  command "sudo bundle install"
+  cwd "/vagrant"
+end
+
+#--------------------------------------------------------------------------------
+# Apache
+#--------------------------------------------------------------------------------
+execute "disable-default-site" do
+  command "sudo a2dissite default"
+  notifies :restart, resources(:service => "apache2")
+end
+
+web_app "application" do
+  template "application.conf.erb"
+  notifies :restart, resources(:service => "apache2")
+end
+
+#--------------------------------------------------------------------------------
+# Files
+#--------------------------------------------------------------------------------
 cookbook_file "/home/vagrant/.bashrc" do
   source "bashrc"
   mode "0644"
@@ -8,4 +59,22 @@ end
 cookbook_file "/home/vagrant/.bash_profile" do
   source "bash_profile"
   mode "0644"
+end
+
+#--------------------------------------------------------------------------------
+# Application Bootstrap
+#--------------------------------------------------------------------------------
+bash "db-bootstrap" do
+  user "vagrant"
+  cwd "/vagrant"
+
+  code <<-CODE
+rake db:create:all
+rake db:migrate
+rake db:test:prepare
+CODE
+
+  not_if do 
+    `mysql -uroot -proot -e "show databases;"`.include?(node[:rails][:app_name])
+  end
 end
--- cookbooks/rails/recipes/default.rb
+++ cookbooks/rails/recipes/default.rb
@@ -1,20 +1,3 @@
-#--------------------------------------------------------------------------------
-# Required recipes
-#--------------------------------------------------------------------------------
-require_recipe "apt"
-require_recipe "build-essential"
-require_recipe "mysql::server"
-require_recipe "apache2"
-
-#--------------------------------------------------------------------------------
-# Required packages
-#--------------------------------------------------------------------------------
-package "zip"
-package "libxslt1-dev"
-
-#--------------------------------------------------------------------------------
-# Create rails project
-#--------------------------------------------------------------------------------
 gem_package "rails" do
   version node[:rails][:version]
 end
@@ -52,63 +35,5 @@ template "/vagrant/config/database.yml" do
   group "vagrant"
   not_if do
     `ls /vagrant/config`.include?("database.yml") 
-  end
-end
-
-#--------------------------------------------------------------------------------
-# Install passenger
-#--------------------------------------------------------------------------------
-require_recipe "passenger_apache2::mod_rails"
-
-#--------------------------------------------------------------------------------
-# Rake incompatability fix
-#--------------------------------------------------------------------------------
-bash "downgrade-rake" do
-  code "gem uninstall rake -v\"~>0.9\""
-
-  only_if do
-    `gem list rake`.include?('0.9')
-  end
-end
-
-#--------------------------------------------------------------------------------
-# Gems
-#--------------------------------------------------------------------------------
-gem_package "bundler"
-
-execute "install bundle" do
-  command "sudo bundle install"
-  cwd "/vagrant"
-end
-
-#--------------------------------------------------------------------------------
-# Apache
-#--------------------------------------------------------------------------------
-execute "disable-default-site" do
-  command "sudo a2dissite default"
-  notifies :restart, resources(:service => "apache2")
-end
-
-web_app "application" do
-  template "application.conf.erb"
-  notifies :restart, resources(:service => "apache2")
-end
-
-
-#--------------------------------------------------------------------------------
-# Application Bootstrap
-#--------------------------------------------------------------------------------
-bash "db-bootstrap" do
-  user "vagrant"
-  cwd "/vagrant"
-
-  code <<-CODE
-rake db:create:all
-rake db:migrate
-rake db:test:prepare
-CODE
-
-  not_if do 
-    `mysql -uroot -proot -e "show databases;"`.include?(node[:rails][:app_name])
   end
 end
