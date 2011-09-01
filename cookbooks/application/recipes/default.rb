63f26327f44e42e9aac9e76d0eb2ed91b9bce66b
Rudiger Lippert <rudy.lippert@originatelabs.com>
2011-09-01 15:40:46 -0700

    Refactored the apache and passenger config into a rails recipe

--- cookbooks/application/recipes/default.rb
+++ cookbooks/application/recipes/default.rb
@@ -1,40 +1,4 @@
-#--------------------------------------------------------------------------------
-# Required recipes
-#--------------------------------------------------------------------------------
 require_recipe "apt"
 require_recipe "build-essential"
-require_recipe "apache2"
 require_recipe "rails::mysql"
-require_recipe "passenger_apache2::mod_rails"
-
-#--------------------------------------------------------------------------------
-# Required packages
-#--------------------------------------------------------------------------------
-#package "zip"
-#package "libxslt1-dev"
-
-#--------------------------------------------------------------------------------
-# Apache
-#--------------------------------------------------------------------------------
-execute "disable-default-site" do
-  command "sudo a2dissite default"
-  notifies :restart, resources(:service => "apache2")
-end
-
-web_app "application" do
-  template "application.conf.erb"
-  notifies :restart, resources(:service => "apache2")
-end
-
-#--------------------------------------------------------------------------------
-# Files
-#--------------------------------------------------------------------------------
-cookbook_file "/home/vagrant/.bashrc" do
-  source "bashrc"
-  mode "0644"
-end
-
-cookbook_file "/home/vagrant/.bash_profile" do
-  source "bash_profile"
-  mode "0644"
-end
+require_recipe "rails::apache_passenger"
