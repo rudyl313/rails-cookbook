222fb10e19874a2d3202c4ced0748bfc80c90a28
Rudiger Lippert <rudy.lippert@originatelabs.com>
2011-09-02 00:33:34 -0700

    Fixed postgresql recipe and made it idempotent

--- cookbooks/rails/recipes/postgresql.rb
+++ cookbooks/rails/recipes/postgresql.rb
@@ -1,6 +1,8 @@
 require_recipe "postgresql::server"
 require_recipe "rails"
 
+package "libpq-dev"
+
 template "#{node[:rails][:root]}/Gemfile" do
   source "Gemfile.erb"
   mode "0666"
@@ -31,6 +33,22 @@ template "#{node[:rails][:root]}/config/database.yml" do
   end
 end
 
+bash "change-cluster-encoding-to-utf8" do
+  code <<-CODE
+pg_dropcluster --stop 8.4 main
+pg_createcluster --start -e UTF-8 8.4 main
+CODE
+  not_if do `sudo sudo -u postgres psql -c '\\l'`.include?(node[:rails][:app_name]) end
+end
+
+bash "create-db-user" do
+  user "postgres"
+  code <<-CODE
+psql -c "create user root with createdb login encrypted password 'root'"
+CODE
+  not_if do `sudo sudo -u postgres psql -c '\\l'`.include?(node[:rails][:app_name]) end
+end
+
 bash "db-bootstrap" do
   user node[:rails][:user]
   cwd node[:rails][:root]
@@ -40,4 +58,5 @@ rake db:create:all
 rake db:migrate
 rake db:test:prepare
 CODE
+  not_if do `sudo sudo -u postgres psql -c '\\l'`.include?(node[:rails][:app_name]) end
 end
